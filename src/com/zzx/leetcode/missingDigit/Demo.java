8ffd87a0834414aa5411c0d22a0629c62d9894ed
zhangzx <zhangzx@allinfinance.com>
2019-05-31 15:03:32 +0800

    LeetCode268数字缺失

---
+++ src/com/zzx/leetcode/missingDigit/Demo.java
@@ -0,0 +1,23 @@
+package com.zzx.leetcode.missingDigit;
+
+/**
+ * @ClassName Demo
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/5/31 14:44
+ * Version 1.0
+ **/
+public class Demo {
+    public static void main(String[] args) {
+       int a = 1;
+       int b = 2;
+       int c = 10;
+       int d = 11;
+       int e = 100;
+       int f = 200;
+        System.out.println(a ^= b);
+        System.out.println(c ^= d);
+//        System.out.println(a ^ b);
+        System.out.println(e ^= f);
+    }
+}
---
+++ src/com/zzx/leetcode/missingDigit/missingDigit.java
@@ -0,0 +1,33 @@
+package com.zzx.leetcode.missingDigit;
+
+/**
+ * @ClassName missingDigit
+ * @Description
+ * @Author zhangzx
+ * @Date 2019/5/31 14:37
+ * Version 1.0
+ **/
+class Solution {
+        public int missingNumber(int[] nums) {
+        int res=0;
+        for (int i=0;i<=nums.length;i++) {
+            res^=i;
+        }
+        for (int i=0;i<nums.length;i++) {
+            res^=nums[i];
+        }
+        return res;
+    }
+}
+
+public class missingDigit {
+
+    public static void main(String[] args) {
+
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+        Solution solution = new Solution();
+        int x = solution.missingNumber(nums);
+        System.out.println(x);
+
+    }
+}
